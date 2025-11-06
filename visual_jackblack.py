b27d00c8b2b5ec1f9bf88dbb53ae0048cad51c27
JOH0031 <joh0031@highvalesc.vic.edu.au>
2025-11-06 13:01:36 +1100

    tkinter main

---
+++ visual_jackblack.py
@@ -0,0 +1,11 @@
+import tkinter as tk 
+from tkinter import *
+from PIL import Image, ImageTk
+
+root = tk.Tk()
+root.title("JackBlack")
+root.config(bg='#004225')
+root.geometry('500x500')
+
+
+root.mainloop()
